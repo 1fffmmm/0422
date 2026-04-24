7f883f00fc4013d7c1929bce30bd5ae12d59b12e
1fffmmm <meism51@gmail.com>
2026-04-25 08:30:36 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,11 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  projectId: "..."
+});
+
+const messaging = firebase.messaging();
