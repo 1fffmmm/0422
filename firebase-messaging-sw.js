e440cdf894b66d1a84df15732013783fe7a4dc23
1fffmmm <meism51@gmail.com>
2026-04-25 10:49:13 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,26 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 
-// main.htmlと同じFirebase設定
-firebase.initializeApp({
-  apiKey: "AIzaSyBCwEb0nAynWlcIYloAwwdDQocXgD1W8DQ",
-  authDomain: "user-cd2c1.firebaseapp.com",
-  projectId: "user-cd2c1",
-  storageBucket: "user-cd2c1.firebasestorage.app",
-  messagingSenderId: "990277200920",
-  appId: "1:990277200920:web:952d974484fa76ed89e76d"
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyBCwEb0nAynWlcIYloAwwdDQocXgD1W8DQ",
+    authDomain: "user-cd2c1.firebaseapp.com",
+    projectId: "user-cd2c1",
+    storageBucket: "user-cd2c1.firebasestorage.app",
+    messagingSenderId: "990277200920",
+    appId: "1:990277200920:web:952d974484fa76ed89e76d"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// バックグラウンドで通知を受け取った時の処理
+// バックグラウンドでのプッシュ通知処理
 messaging.onBackgroundMessage((payload) => {
-  console.log('バックグラウンド通知を受信:', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon.png' // アイコンがあれば指定
+    icon: '/icon.png' // 通知アイコンのパスがあれば指定
   };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
