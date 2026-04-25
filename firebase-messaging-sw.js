fd77aaea37567276eca7b60ea7627fb4202b169c
1fffmmm <meism51@gmail.com>
2026-04-25 09:08:00 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,9 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "...",
-  // index.htmlと同じ設定をここに貼る
-});
+      apiKey: "AIzaSyBCwEb0nAynWlcIYloAwwdDQocXgD1W8DQ",
+      authDomain: "user-cd2c1.firebaseapp.com",
+      projectId: "user-cd2c1",
+      storageBucket: "user-cd2c1.firebasestorage.app",
+      messagingSenderId: "990277200920",
+      appId: "1:990277200920:web:952d974484fa76ed89e76d",
+      measurementId: "G-30QNZ04PDZ"
+    };
 
 const messaging = firebase.messaging();
 
